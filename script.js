a45b24f3e6df40c47664dd3695d474edf07b9287
suprafox13-source <suprafox13@gmail.com>
2026-02-02 11:56:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwl2xoth2_jFKGvCHb-NDqS6IXhakGRQM",
+  apiKey: "API_KEYİN",
   authDomain: "pyerista.firebaseapp.com",
   projectId: "pyerista",
 };
@@ -15,31 +15,22 @@ const profilAlan = document.getElementById("profilAlan");
 const profilResim = document.getElementById("profilResim");
 const cikisBtn = document.getElementById("cikisBtn");
 
-// Giriş kontrolü
 onAuthStateChanged(auth, (user) => {
   if (user) {
     uyeBtn.style.display = "none";
     profilAlan.style.display = "block";
-
-    // Firebase profil foto varsa göster
-    if (user.photoURL) {
-      profilResim.src = user.photoURL;
-    }
   } else {
     uyeBtn.style.display = "inline-block";
     profilAlan.style.display = "none";
   }
 });
 
-// Profil resmine tıkla → çıkış butonu
 profilResim.onclick = () => {
   cikisBtn.style.display =
     cikisBtn.style.display === "none" ? "block" : "none";
 };
 
-// Çıkış
 cikisBtn.onclick = async () => {
   await signOut(auth);
   location.reload();
 };
-
